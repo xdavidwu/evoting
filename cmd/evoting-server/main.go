a6ce4639a28da9771f4cda8443d8c8978ea7f020
xdavidwu <xdavidwuph@gmail.com>
2023-03-25 19:59:58 +0800

    evoting-server: impl grpc listenings

--- cmd/evoting-server/main.go
+++ cmd/evoting-server/main.go
@@ -1,8 +1,44 @@
 package main
 
 import (
-	//pb "github.com/xdavidwu/evoting/proto"
+	"flag"
+	"log"
+	"net"
+	"google.golang.org/grpc"
+	pb "github.com/xdavidwu/evoting/proto"
 )
 
+var (
+	registAddr	= flag.String("registration-listen", "localhost:1234", "Listen address for registration")
+	voteAddr	= flag.String("vote-listen", "0.0.0.0:5678", "Listen address for voting")
+)
+
+type registrationServer struct {
+	pb.UnimplementedRegistrationServer
+}
+
+type eVotingServer struct {
+	pb.UnimplementedEVotingServer
+}
+
 func main() {
+	flag.Parse()
+
+	registLn, err := net.Listen("tcp", *registAddr)
+	if err != nil {
+		log.Fatalf("failed to listen %s: %v", *registAddr, err)
+	}
+	voteLn, err := net.Listen("tcp", *voteAddr)
+	if err != nil {
+		log.Fatalf("failed to listen %s: %v", *voteAddr, err)
+	}
+
+	registServer := grpc.NewServer()
+	voteServer := grpc.NewServer()
+
+	pb.RegisterRegistrationServer(registServer, &registrationServer{})
+	pb.RegisterEVotingServer(voteServer, &eVotingServer{})
+
+	go registServer.Serve(registLn)
+	voteServer.Serve(voteLn)
 }
